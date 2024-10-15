6df0ed3b84e295a0d63f6acf4a293383c16e24b2
caru-ini <caru17122@gmail.com>
2024-10-15 12:56:14 +0000

    feat: use useTransition instead

--- src/components/logout.tsx
+++ src/components/logout.tsx
@@ -2,27 +2,26 @@
 import { Button } from '@/components/ui/button';
 import { client } from '@/lib/auth-client';
 import { useRouter } from 'next/navigation';
-import { useState } from 'react';
+import { useTransition } from 'react';
 import { LuLoader2, LuLogOut } from 'react-icons/lu';
 
 export const LogoutButton: React.FC = () => {
   const router = useRouter();
-  const [isLoading, setIsLoading] = useState(false);
-
+  const [isPending, startTransition] = useTransition();
   const handleLogout = async () => {
-    setIsLoading(true);
     try {
-      await client.signOut({ fetchOptions: { onSuccess: () => router.push('/signin') } });
+      await client.signOut();
+      startTransition(() => {
+        router.push('/signin');
+      });
     } catch (error) {
       console.error('ログアウト中にエラーが発生しました:', error);
-    } finally {
-      setIsLoading(false);
     }
   };
 
   return (
     <Button variant='ghost' size='icon' onClick={handleLogout}>
-      {isLoading ? (
+      {isPending ? (
         <LuLoader2 className='size-[1.2rem] animate-spin' />
       ) : (
         <LuLogOut className='size-[1.2rem]' />
