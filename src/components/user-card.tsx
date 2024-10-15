02c142d1ab78c44f2c6011d7a80ec38abdc21077
caru-ini <caru17122@gmail.com>
2024-10-15 13:12:01 +0000

    feat: use dropdown-menu in user-card

--- src/components/user-card.tsx
+++ src/components/user-card.tsx
@@ -3,17 +3,31 @@
 import { Avatar, AvatarFallback, AvatarImage } from '@/components/ui/avatar';
 import { Button } from '@/components/ui/button';
 import { Card, CardContent, CardFooter, CardHeader } from '@/components/ui/card';
+import {
+  DropdownMenu,
+  DropdownMenuContent,
+  DropdownMenuItem,
+  DropdownMenuTrigger,
+} from '@/components/ui/dropdown-menu';
 import { client } from '@/lib/auth-client';
-import Link from 'next/link';
-import { useState } from 'react';
-import { LuChevronDown } from 'react-icons/lu';
+import { ChevronDown, Loader2 } from 'lucide-react';
+import { useRouter } from 'next/navigation';
+import { useTransition } from 'react';
 
 type UserCardProps = {
   user?: typeof client.$Infer.Session.user;
 };
 
 export const UserCard: React.FC<UserCardProps> = ({ user }) => {
-  const [isMoreOptionsOpen, setIsMoreOptionsOpen] = useState(false);
+  const router = useRouter();
+  const [isPending, startTransition] = useTransition();
+
+  const onSignOut = async () => {
+    await client.signOut();
+    startTransition(() => {
+      router.push('/signin');
+    });
+  };
   return (
     <Card className='w-full max-w-[500px]'>
       {user ? (
@@ -30,20 +44,22 @@ export const UserCard: React.FC<UserCardProps> = ({ user }) => {
             <h2 className='text-2xl font-bold'>{user?.name || 'ユーザー名'}</h2>
             <p className='text-muted-foreground'>{user?.email || 'メールアドレス'}</p>
           </CardContent>
-          <CardFooter className='flex flex-col justify-between gap-2'>
-            <div className='flex w-full justify-between gap-2'>
-              <Button variant='secondary'>ログアウト</Button>
-              <Button variant='outline' onClick={() => setIsMoreOptionsOpen(!isMoreOptionsOpen)}>
-                オプション <LuChevronDown className='size-4' />
-              </Button>
-            </div>
-            {isMoreOptionsOpen && (
-              <div className='flex flex-col gap-2'>
-                <Link href='/set-password' passHref>
-                  <Button variant='secondary'>パスワードを設定</Button>
-                </Link>
-              </div>
-            )}
+          <CardFooter className='flex justify-between gap-2'>
+            <Button variant='secondary' onClick={onSignOut} disabled={isPending}>
+              {isPending ? <Loader2 className='size-4 animate-spin' /> : 'ログアウト'}
+            </Button>
+            <DropdownMenu>
+              <DropdownMenuTrigger asChild>
+                <Button variant='secondary'>
+                  <ChevronDown className='size-4' /> 詳細設定
+                </Button>
+              </DropdownMenuTrigger>
+              <DropdownMenuContent>
+                <DropdownMenuItem onClick={() => router.push('/set-password')}>
+                  パスワードを設定
+                </DropdownMenuItem>
+              </DropdownMenuContent>
+            </DropdownMenu>
           </CardFooter>
         </>
       ) : (
