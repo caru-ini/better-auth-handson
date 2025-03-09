6cfc6fdeca45ed66af14d0d13048486b8625c240
caru-ini <caru17122@gmail.com>
2025-03-09 12:01:20 +0000

    chore: initialize project with nextjs 15

--- src/lib/auth-client.ts
+++
@@ -1,7 +0,0 @@
-import { createAuthClient } from 'better-auth/react';
-
-export const client = createAuthClient({
-  baseURL: 'http://localhost:3000',
-});
-
-export const { signIn, signUp, useSession, signOut } = client;
